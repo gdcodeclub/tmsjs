d8bcf951b928b367cbbd9e78006e28ddbef19b80
Eric Ebbesen <eebbesen@eebbesen-mbp.local>
2017-07-02 07:02:06 -0500

    var to const

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const methodOverride = require('method-override')
 const request = require('request')
 const tmsRoutes = require('./routes/tms')
 const port = process.env.PORT ||  8080
+<<<<<<< HEAD
+=======
+const ip = process.env.IP || 'localhost'
+>>>>>>> var to const
 
 const mongoose = require('mongoose')
 const databaseUrl = process.env.DATABASEURL || 'mongodb://localhost/tmsjs'
@@ -17,8 +21,13 @@ app.use(express.static(__dirname + '/public'))
 app.use(methodOverride('_method'))
 app.use(tmsRoutes)
 
+<<<<<<< HEAD
 const server = app.listen(port, function(){
   console.log('listening at ' + port)
+=======
+const server = app.listen(port, ip, function(){
+  console.log('listening at ' + ip + ':' + port)
+>>>>>>> var to const
 })
 
 module.exports = server
