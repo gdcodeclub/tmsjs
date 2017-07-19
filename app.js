aa8799e2e236e1844790c95e1d1f38f316ded8c3
Eric Ebbesen <eebbesen@eebbesen-mbp.local>
2017-07-19 05:30:02 -0500

    removing unused constant

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const methodOverride = require('method-override')
 const request = require('request')
 const tmsRoutes = require('./routes/tms')
 const port = process.env.PORT ||  8080
-<<<<<<< HEAD
-=======
-const ip = process.env.IP || 'localhost'
->>>>>>> var to const
 
 const mongoose = require('mongoose')
 const databaseUrl = process.env.DATABASEURL || 'mongodb://localhost/tmsjs'
@@ -21,13 +17,8 @@ app.use(express.static(__dirname + '/public'))
 app.use(methodOverride('_method'))
 app.use(tmsRoutes)
 
-<<<<<<< HEAD
 const server = app.listen(port, function(){
   console.log('listening at ' + port)
-=======
-const server = app.listen(port, ip, function(){
-  console.log('listening at ' + ip + ':' + port)
->>>>>>> var to const
 })
 
 module.exports = server
